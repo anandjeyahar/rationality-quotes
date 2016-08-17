af5c5b83a5be5abd8b061b6ef317e563198b0f07
anand <anand.jeyahar@gmail.com>
2016-08-17 21:41:47 +0530

    minor changes..

--- rationality_quotes.sh
+++ rationality_quotes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cat urls | grep 2012 | while read url ; do f=`echo $url | cut -f4 -d'/'` ; wget -O - $url > $f ; done
-cat urls | grep 2012 | python parse.py pretty > rq.html
+cat urls | grep $1 | while read url ; do f=`echo $url | cut -f4 -d'/'` ; wget -O - $url > $f ; done
+cat urls | grep $1 | python parse.py pretty > rq.html
