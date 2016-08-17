3282ad4b187defd1265fb1b7884dff57f1fb5a36
anand <anand.jeyahar@gmail.com>
2016-08-17 20:19:23 +0530

    more stuff..

---
+++ css/a.js
@@ -0,0 +1,10 @@
+if (window['google'] != undefined && window['google']['loader'] != undefined) {
+if (!window['google']['feeds']) {
+window['google']['feeds'] = {};
+google.feeds.Version = '1.0';
+google.feeds.JSHash = '4af0f9d507d970221e7badc56cb6f35a';
+google.feeds.LoadArgs = 'file\75feeds\46v\0751';
+}
+google.loader.writeLoadTag("css", google.loader.ServiceBase + "/api/feeds/1.0/4af0f9d507d970221e7badc56cb6f35a/default.css", false);
+google.loader.writeLoadTag("script", google.loader.ServiceBase + "/api/feeds/1.0/4af0f9d507d970221e7badc56cb6f35a/default+en.I.js", false);
+}
